65e4ebca0b5b720d4b9bf0edde8335f6091934df
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-10-02 14:44:55 +0200

    Changes

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,3 +2,10 @@
 import loadMultipleMeals from './modules/themealdb/themealdm.js';
 
 loadMultipleMeals()
+
+let darkbutton = document.getElementById('darkmode');
+
+function darkbuttonclick() {
+    document.body.classList.toggle('darkmode');
+}
+darkbutton.addEventListener('click', darkbuttonclick);
